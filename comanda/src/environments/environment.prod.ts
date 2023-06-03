75877c3a5fb65676c3650365cad4972f448ddc04
Federico Dacal <federicodacal@gmail.com>
2023-06-03 13:06:10 -0300

    firebase

--- comanda/src/environments/environment.prod.ts
+++ comanda/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'pps-2do-parcial',
+    appId: '1:34748268613:web:bd1cb6a0a701b94161e32c',
+    storageBucket: 'pps-2do-parcial.appspot.com',
+    apiKey: 'AIzaSyCNr5Efj4hyqSfi0l7Z_N4XnnHRa_1pT1s',
+    authDomain: 'pps-2do-parcial.firebaseapp.com',
+    messagingSenderId: '34748268613',
+  },
   production: true
 };
--- comanda/src/environments/environment.ts
+++ comanda/src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'pps-2do-parcial',
+    appId: '1:34748268613:web:bd1cb6a0a701b94161e32c',
+    storageBucket: 'pps-2do-parcial.appspot.com',
+    apiKey: 'AIzaSyCNr5Efj4hyqSfi0l7Z_N4XnnHRa_1pT1s',
+    authDomain: 'pps-2do-parcial.firebaseapp.com',
+    messagingSenderId: '34748268613',
+  },
   production: false
 };
 
